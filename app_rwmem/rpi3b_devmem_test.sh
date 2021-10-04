dab2451535bae6d0e33802052c23fe39727919f6
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2021-10-04 14:16:29 +0530

    initial commit

---
+++ app_rwmem/rpi3b_devmem_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# R Pi 3B[+]: devmem_rw load helper script
+### NOTE ###
+# The values in this test script are particular to the hardware;
+# the Raspberry Pi 3B[+]
+############
+BASE_ADDR=0x7e00b000    # R Pi 3B interrupt registers
+OFFSET=0x200            # offset from base address to start mapping
+   # Ref: Broadcom 2835 (/2837) ARM Peripherals.pdf doc
+LEN=0x28            # bytes
+IOMEM_NAME=rpi_intr  # in /proc/iomem
+FORCE_REL=0
+
+sudo rmmod devmem_rw 2>/dev/null
+sudo dmesg -C
+let BUSADDR=${BASE_ADDR}+${OFFSET}
+printf "devmem_rw: BASEADDR = 0x%x, len=%d bytes, name=%s\n" ${BUSADDR} ${LEN} ${IOMEM_NAME}
+cmd="sudo insmod ../drv_rwmem/devmem_rw.ko iobase_start=${BUSADDR} iobase_len=${LEN} reg_name=${IOMEM_LBL} force_rel=${FORCE_REL}"
+#echo ${cmd}
+eval ${cmd} || exit 1
+lsmod|grep devmem_rw
+sudo dmesg
+sudo grep "${IOMEM_NAME}" /proc/iomem
