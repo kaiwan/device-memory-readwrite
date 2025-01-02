c43fdad0dfd118a463351c6152d337e5a867a72d
Kaiwan Billimoria <kaiwan.billimoria@gmail.com>
2025-01-02 12:45:28 +0530

    rpi4b_devmemrw_test.sh: cleanup script; calculate region addr, len & then test

--- tests/rpi4b_devmemrw_test.sh
+++ tests/rpi4b_devmemrw_test.sh
@@ -9,26 +9,32 @@ die()
 echo >&2 "FATAL: $*" ; exit 1
 }
 
+hash bc || die "Please install the bc(1) util"
 KDRV_LOC=../drv_rwmem
 KDRV=devmem_rw
-BASE_ADDR=0xfe00b880   # one of the R Pi 4B mailbox registers, as an example region to view...
-OFFSET=0               # offset from base address to start mapping
-   # Ref: Broadcom 2835 (/2837) ARM Peripherals.pdf doc
-LEN=64                 # bytes
-IOMEM_NAME=my_mailbox  # in /proc/iomem
-FORCE_REL=1
-
 APP_LOC=../app_rwmem
 RDMEM=${APP_LOC}/rdmem
 WRMEM=${APP_LOC}/wrmem
 [[ ! -f ${KDRV_LOC}/${KDRV}.ko ]] && die "First build the ${KDRV_LOC}/${KDRV}.ko module"
 [[ ! -f ${RDMEM} ]] && die "First build the ${RDMEM} app"
-[[ ! -f ${WRMEM} ]] && die "First build the ${WRMEM} app"
+#[[ ! -f ${WRMEM} ]] && die "First build the ${WRMEM} app"
 
-BASE_ADDR2=${BASE_ADDR:2}
-sudo grep "${BASE_ADDR2}" /proc/iomem || {
-  echo "MMIO region from ${BASE_ADDR} not found? aborting..." ; exit 1
-}
+REGION=watchdog
+   # Ref: Broadcom 2835 (/2837) ARM Peripherals.pdf doc
+IOMEM_NAME=my_${REGION}  # in /proc/iomem
+OFFSET=0               # offset from base address to start mapping
+FORCE_REL=1
+
+sudo grep ${REGION} /proc/iomem |tail -n1 || die "can't find appropriate IO region (${REGION})"
+BASE_ADDR=$(sudo grep watchdog /proc/iomem |tail -n1|cut -d: -f1|cut -d- -f1|xargs)
+BASE_ADDR_END=$(sudo grep watchdog /proc/iomem |tail -n1|cut -d: -f1|cut -d- -f2|xargs)
+BASE_ADDR=0x${BASE_ADDR^^}
+BASE_ADDR_END=0x${BASE_ADDR_END^^}
+LEN=$(printf "%u" $((${BASE_ADDR_END}-${BASE_ADDR}+1)))
+
+#sudo grep "${BASE_ADDR2}" /proc/iomem || {
+#  echo "MMIO region from ${BASE_ADDR} not found? aborting..." ; exit 1
+#}
 
 sudo rmmod ${KDRV} 2>/dev/null
 sudo dmesg -C
@@ -44,7 +50,7 @@ eval "${CMD}" || {
 lsmod|grep ${KDRV}
 sudo dmesg
 sudo grep "${IOMEM_NAME}" /proc/iomem
-sudo ../app_rwmem/rdmem -o 0 ${LEN} || {
+sudo ${RDMEM} -o 0 ${LEN} || {
   sudo rmmod ${KDRV} 2>/dev/null
   echo "*fail*" ; exit 1
 }
